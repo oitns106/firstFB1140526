5ba231b0081c28f34efa436b90f7f71f82593510
oitns106 <111697732+oitns106@users.noreply.github.com>
2025-05-31 14:58:52 +0800

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,69 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'package:firebase_database/firebase_database.dart';
-import 'firebase_options.dart';
-import 'package:intl/intl.dart';
-
-Future<void> main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
-  runApp(const hospital1());
-}
-
-class hospital1 extends StatefulWidget {
-  const hospital1({super.key});
-
-  @override
-  State<hospital1> createState() => _hospital1State();
-}
-
-class _hospital1State extends State<hospital1> {
-
-  String ms1='病人準備中';
-  final DatabaseReference ref=FirebaseDatabase.instance.ref('patient/patient1');
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Container(
-            child: ElevatedButton(
-              child: Text(ms1),
-              onPressed: () async {
-                if (ms1=='病人準備中') ms1='病人已進入手術室';
-                else if (ms1=='病人已進入手術室') ms1='病人已開始手術';
-                else if (ms1=='病人已開始手術') ms1='病人已結束手術進入恢復室';
-                else if (ms1=='病人已結束手術進入恢復室') ms1='病人已回到病房';
-                else if (ms1=='病人已回到病房') ms1='病人準備中';
-                String cdate=DateFormat('yyyy-MM-dd HH:mm:ss').format(DateTime.now());
-                await ref.set(cdate+ms1);
-                setState(() {});
-              },
-            ),
-          ),
-        ),
-      ),
-    );
-  }
-}
-
-
-
-
-
-
-
-
-
-
-
-
-/*
-
-import 'package:flutter/material.dart';
-import 'package:firebase_core/firebase_core.dart';
-import 'firebase_options.dart';
+import 'firebase_options.dart';                       //請同學們自行產生firebase_options.dart
 import 'new_account.dart';
 import 'login.dart';
 import 'service.dart';
@@ -133,7 +70,3 @@ class _MyHomePageState extends State<MyHomePage> {
     );
   }
 }
-
-
-
- */
